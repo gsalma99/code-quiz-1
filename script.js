68fb2c6a35e01852cb802abed9791f2dfec5a1eb
gsalma99 <salmagonzalezzz10011@gmail.com>
2022-09-13 22:29:31 -0500

    third update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var quizPage = document.getElementById("quiz-page");
 var countdownTimer = document.getElementById("timer");
 var startGame = document.getElementById("start-game");
 var highScorePage = document.getElementById("high-score-list");
-var submitQuiz = document.getElementById("highest-score");
+var submitScore = document.getElementById("highest-score");
 
 questionIndex = 0;
 var questions = [
@@ -27,29 +27,97 @@ var questions = [
         answer: 3
     },
 ];
+var lastQuestion = questions[2].question;
 
 var timeLeft;
 var questionIndex;
 var quizTimer; 
+function checkAnswer(event) {}
 
 function startQuiz() {
     console.log("I need to start the quiz");
     timeLeft = 60
-    var quizTimer = setInterval(function() {
-        timeLeft --;
-        countdownTimer.textContent = timeLeft;
-    } ,1000);
+    startTimers();
+    startGame.style.display ="none";
+    quizPage.style.display ="block";
     displayQuestion();
 }
-// function displayQuestion() {
-//     var question = document.createElement("h1");
 
+function startTimer() {
+    quizTimer = setInterval(function() {
+        timeLeft --;
+        countdownTimer.textContent = timeLeft;
+    } ,1000)
+}
 
-// }
+function endGame() {
+    clearInterval(quizTimer);
+    quizPage.style.display = "none";
+    highScorePage.style.display ="block";
+    document.getElementById("score").textContent = timeLeft;
+}
 
+function submitScore() {
+    var initals = document.getElementById("initals").value;
+    var allHighScore = localStorage.getItem("high-score");
+    if (!allHighScore) {
+        allHighScore = []
+    }
+    else {
+        allHighScore= JSON.parse(allHighScore)
+    }
+    var newPerson = {
+        initals: initals,
+        score: timeLeft,
+    }
+    
+    allHighScore.push(newPerson)
+    submitQuiz.innerHTML=""
+    allHighScore.forEach(scoreObject => {
+        var highScoreDiv= document.createElement("p")
+        highScoreDiv.textContent = scoreObject.initals + " : " + scoreObject.score
+        submitQuiz.appendChild(highScoreDiv) 
+    });
+    localStorage.setItem(
+        "high-score", JSON.stringify(allHighScore)
+    )
+}
 
+function choicesSelector(event) {
+    var userChoice = event.target.textContent;
+    var currentAnswer = questions[questionIndex].answer;
+    if (userChoice !== currentAnswer) {
+        timeLeft -= 5;
+        console.log(timeLeft);
+    }
+    if (lastQuestion !== questions[questionIndex].question) {
+        questionIndex++
+    } else {
 
+        endGame()
 
+    }
+    quizPage.innerHTML=""
+    displayQuestion ()
+}
+
+function displayQuestion() {
+    var question = document.createElement("h1");
+    question.textContent = questions[questionIndex].question;
+    quizPage.appendChild(question);
+
+    for (
+        let index = 0;
+        index < questions[questionIndex].choices.length;
+        index++
+    ) {
+        const choice = questions[questionIndex].choices[index];
+        var questionButton = document.createElement("button");
+        questionButton.textContent = choice;
+        questionButton.addEventListener("click", choicesSelector);
+        quizPage.appendChild(questionButton);
+    }
+}
 
 
 
